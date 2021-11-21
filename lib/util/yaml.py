6dfa81a76d16fbee18686125ea7df381dbb93cab
zincentimeter <zincentimeter@outlook.com>
2021-11-22 01:09:53 +0800

    misc: cancel meaningless staticmethod

--- lib/util/yaml.py
+++ lib/util/yaml.py
@@ -2,7 +2,6 @@ from os import path
 import yaml
 
 # extract args from file
-@staticmethod
 def yaml_to_dict(yaml_path : path) -> dict[str, str]:
     with open(yaml_path, mode='r') as yaml_file:
         config = yaml.load(yaml_file, Loader=yaml.FullLoader)

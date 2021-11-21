5d6a4ecb3581f9f59c47ec109cbf3bb887c86ca2
zincentimeter <zincentimeter@outlook.com>
2021-11-22 01:10:15 +0800

    net: impl twitter download

---
+++ lib/net/twitter.py
@@ -0,0 +1,28 @@
+import tweepy
+
+from lib.util.exception import MetaException
+
+class TwitterError(MetaException): 
+    pass
+
+class API_ver_1():
+
+    def get_auth_handler(consumer_key : str,
+                        consumer_secret : str,
+                        access_token : str,
+                        access_token_secret : str,
+                        **kwargs) -> tweepy.OAuthHandler:
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        return auth
+
+    def get_api_instance(proxy : str,
+                    **kwargs) -> tweepy.API:
+        try:
+            auth = API_ver_1.get_auth_handler(**kwargs['secret'])
+        except TypeError:
+            raise TwitterError('Wrong config level or key.')
+        except KeyError:
+            raise TwitterError('Section secret not found in the config.')
+        return tweepy.API(auth=auth, proxy=proxy)
+
---
+++ twitter_pic_save.py
@@ -0,0 +1,29 @@
+from lib.net import twitter, stream
+from lib.util import yaml
+from lib.file import path
+
+
+if (__name__ == '__main__'):
+    minosiro_conf = yaml.yaml_to_dict('conf/minosiro.yaml')
+    twi_conf = yaml.yaml_to_dict('conf/twitter.yaml')
+    
+    twi_instance = twitter.API_ver_1.get_api_instance(
+        proxy=minosiro_conf['proxy']['https'], **twi_conf)
+    
+    liked_tweets = twi_instance.get_favorites(
+        screen_name='zincentimeter', count=5)
+    
+    for tweet in liked_tweets:
+                
+        if (not hasattr(tweet, 'extended_entities')):
+            continue
+        
+        if ('media' not in tweet.extended_entities):
+            continue
+        
+        for media_file in tweet.extended_entities['media']:
+            file_url = media_file['media_url_https']
+            stream.download(file_url, 
+                        path.get_type_path('raw_data',
+                                           **minosiro_conf['directory']),
+                        proxy_conf=minosiro_conf['proxy'])
